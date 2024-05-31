59d4e48657967094a8968b7efd0382562a829013
Ludwring Liccien <ludwring.liccien@gmail.com>
2024-05-31 13:53:01 -0500

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lliccien.github.io',
-    base: 'altamiranda-landing'
+    base: 'altamiranda-landing',
+    output: 'static'
 
 });
